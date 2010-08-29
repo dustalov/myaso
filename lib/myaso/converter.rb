6cca063009b9d758b0d1d004399d276b2d780454
Dmitry A. Ustalov <dmitry@eveel.ru>
2010-08-29 18:03:24 +0600

    Converter's core is ready

--- lib/myaso/converter.rb
+++ lib/myaso/converter.rb
@@ -5,30 +5,40 @@ class Myaso
     require 'iconv'
     require 'tempfile'
 
-    attr_reader :tch_path, :morphs, :gramtab, :encoding
+    attr_reader :storage_path, :morphs, :gramtab, :encoding
 
     # Create new <tt>Myaso::Converter</tt> with defined
-    # result <tt>tch_path</tt> and <tt>options</tt>
-    # from <tt>Myaso::CLI</tt>.
-    def initialize(tch_path, morphs, gramtab, options)
-      @tch_path, @morphs, @gramtab, @encoding =
-        tch_path, morphs, gramtab, options[:encoding]
+    # result path in <tt>storage_path</tt> and following
+    # <tt>options</tt> from <tt>Myaso::CLI</tt>.
+    def initialize(storage_path, morphs, gramtab, options)
+      @storage_path, @morphs, @gramtab, @encoding =
+        storage_path, morphs, gramtab, options[:encoding]
     end
 
     # Convert the http://aot.ru dictonaries to
-    # <tt>myaso</tt>-compatible format (TokyoCabinet Hash).
+    # <tt>myaso</tt>-eatable format.
     def perform!
       store # just initialize TC
 
       # load morphs
       puts "Processing '#{morphs}'..."
       to_tempfile(morphs).tap do |file|
+        file.rewind
+
         puts '  Loading rules...'
         load_rules(file)
-        puts '  Loading lemmas...'
-        load_lemmas(file)
+
+        puts '  Passing accents...'
+        morphs_foreach(file)
+
+        puts '  Passing logs...'
+        morphs_foreach(file)
+
         puts '  Loading prefixes...'
         load_prefixes(file)
+
+        puts '  Loading lemmas...'
+        load_lemmas(file)
       end.close!
       puts "Done."
 
@@ -61,9 +71,9 @@ class Myaso
     # file structure with <tt>morphs_file</tt> processing,
     # like “ё” (YO) letter replacing with “е” letter (YE).
     def morphs_foreach(morphs_file, &block)
-      morphs_file.rewind
       morphs_file.readline.to_i.times do |i|
         line = morphs_file.readline
+        next unless block
         line.mb_chars.gsub!('Ё', 'Е')
         block.call(line.strip, i)
       end
@@ -78,9 +88,7 @@ class Myaso
             parts << '' while parts.size < 3
             parts[1].mb_chars.slice! 0..2
 
-            rule = store["rules-#{index}"] || []
-            rule << parts
-            store["rules-#{index}"] = parts
+            store.rules.store(index, parts, :dup)
           end
         end
       end
@@ -89,14 +97,12 @@ class Myaso
     # Parse the <tt>morphs_file</tt> and load it as lemmas section.
     def load_lemmas(file)
       morphs_foreach(file) do |line, index|
-        base, rule_id = line.split
+        record = line.split
+        base, rule_id = record[0], record[1].to_i
 
-        lemmas = store["lemmas-#{base}"] || []
-        lemmas << rule_id.to_i
-        store["lemmas-#{base}"] = lemmas
-
-        freq = store["rulefreq-#{rule_id}"] || 0
-        store["rulefreq-#{rule_id}"] = freq + 1
+        store.lemmas.store(base, rule_id, :dup)
+        freq = (store.rulefreq.fetch(rule_id) { 0 }).to_i
+        store.rulefreq.store(rule_id, freq + 1)
       end
     end
 
@@ -105,7 +111,7 @@ class Myaso
       prefixes = []
       morphs_foreach(file) do |line, index|
         prefixes << line
-        store["prefixes-#{index}"] = line
+        store.prefixes[index] = line
       end
     end
 
@@ -117,7 +123,9 @@ class Myaso
           gram = line.split
           gram << '' while gram.size < 4
           ancode, letter, type, info = gram
-          store["gramtab-#{ancode}"] = [ letter, type, info ]
+
+          store.gramtab[ancode] = { 'letter' => letter,
+            'type' => type, 'info' => info }
         end
       end
     end
@@ -125,9 +133,7 @@ class Myaso
     protected
     # Caching accessor to the TokyoCabinet Hash.
     def store
-      @store ||= TokyoCabinet::HDB.new.tap do |hdb|
-        hdb.open(tch_path)
-      end
+      @store ||= Myaso::Store.new(storage_path)
     end
   end
 end
