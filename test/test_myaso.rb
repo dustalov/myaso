5a5492a8dddb1f59b412148a77e690c4a0a269d2
Dmitry A. Ustalov <dmitry@eveel.ru>
2011-01-27 14:12:06 +0500

    Remove the test directory

--- test/test_myaso.rb
+++
@@ -1,8 +0,0 @@
-# encoding: utf-8
-
-require 'helper'
-
-class TestMyaso < Test::Unit::TestCase
-  context 'Myaso' do
-  end
-end
--- test/test_normalizer.rb
+++
@@ -1,8 +0,0 @@
-# encoding: utf-8
-
-require 'helper'
-
-class TestNormalizer < Test::Unit::TestCase
-  context 'Myaso Normalizer' do
-  end
-end
