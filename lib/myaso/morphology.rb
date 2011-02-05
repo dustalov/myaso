d29cbc5394f37b973f21e6bd4b21e5bcbc1b4232
Dmitry A. Ustalov <dmitry@eveel.ru>
2011-02-05 22:25:59 +0500

    Morphology: predict_by_lemma

--- lib/myaso/morphology.rb
+++ lib/myaso/morphology.rb
@@ -25,51 +25,95 @@ class Myaso::Morphology
   # actual word morphology.
   #
   def predict(word)
-    # TODO: more prediction methods
-    predict_by_suffix(word)
+    gram = predict_by_lemma(word)
+
+    if gram.empty?
+      gram = predict_by_suffix(word)
+    end
+
+    gram.sort do |g1, g2|
+      store.flexias[g1.flexia_id].freq <=>
+        store.flexias[g1.flexia_id].freq
+    end.reverse
   end
 
   private
-  def predict_by_suffix(word) # :nodoc:
-    gram = []
+    def predict_by_lemma(word) # :nodoc:
+      gram = []
 
-    5.downto 1 do |i|
-      word_end_mb = word.mb_chars[-i..-1]
-      next unless word_end_mb
-
-      word_end = word_end_mb.upcase.to_s
-
-      if store.endings.key?(word_end)
-        ending = store.endings[word_end]
-
-        ending.paradigms.each do |flexia_id, forms|
-          flexia = store.flexias[flexia_id]
-
-          forms.each do |id|
-            form = flexia.forms[id]
-            suffix, ancode = form.suffix, form.ancode
-            graminfo = store.ancodes[ancode]
-
-            if Myaso::Constants::PRODUCTIVE_CLASSES.include? graminfo.part
-              suffix_offset = suffix.mb_chars.size + 1
-              lemma = word.mb_chars[0..-suffix_offset].upcase.to_s
-              normal = lemma + flexia.forms[0].suffix
-              method_call = "#{__method__}('#{word_end}')"
-              grammems = graminfo.grammems.split(',')
-
-              gram << Myaso::Model::Gram.new(normal, graminfo.part,
-                grammems, flexia_id, ancode, lemma, method_call)
-            end
-          end
+      store.prefixes['prefixes'].each do |prefix|
+        if word.mb_chars.upcase.start_with? prefix
+          word = word[prefix.size..-1]
         end
       end
 
-      break unless gram.empty?
+      word.size.downto 0 do |i|
+        word_begin = word[0..i].mb_chars.upcase.to_s
+        if lemma = store.lemmas[word_begin]
+          word_end = word[i + 1..-1].mb_chars.upcase.to_s
+
+          flexia_id = lemma.flexia_id
+          flexia = store.flexias[flexia_id]
+
+          flexia.forms.each do |form|
+            suffix, ancode = form.suffix, form.ancode
+            next unless suffix == word_end
+
+            graminfo = store.ancodes[ancode]
+
+            suffix_offset = suffix.mb_chars.size + 1
+            normal = word_begin + flexia.forms[0].suffix
+            method_call = "#{__method__}('#{word.mb_chars.upcase}')"
+            grammems = graminfo.grammems.split(',')
+
+            gram << Myaso::Model::Gram.new(normal, graminfo.part,
+              grammems, flexia_id, ancode, word_begin, method_call)
+          end
+
+          return gram
+        end
+      end
+
+      []
     end
 
-    # expirement: try to sort grams by flexia frequency descending order
-    gram.sort do |g1, g2|
-      store.flexias[g1.flexia_id].freq <=> store.flexias[g1.flexia_id].freq
-    end.reverse
-  end
+    def predict_by_suffix(word) # :nodoc:
+      gram = []
+
+      5.downto 1 do |i|
+        word_end_mb = word.mb_chars[-i..-1]
+        next unless word_end_mb
+
+        word_end = word_end_mb.upcase.to_s
+
+        if store.endings.key?(word_end)
+          ending = store.endings[word_end]
+
+          ending.paradigms.each do |flexia_id, forms|
+            flexia = store.flexias[flexia_id]
+
+            forms.each do |id|
+              form = flexia.forms[id]
+              suffix, ancode = form.suffix, form.ancode
+              graminfo = store.ancodes[ancode]
+
+              if Myaso::Constants::PRODUCTIVE_CLASSES.include? graminfo.part
+                suffix_offset = suffix.mb_chars.size + 1
+                lemma = word.mb_chars[0..-suffix_offset].upcase.to_s
+                normal = lemma + flexia.forms[0].suffix
+                method_call = "#{__method__}('#{word_end}')"
+                grammems = graminfo.grammems.split(',')
+
+                gram << Myaso::Model::Gram.new(normal, graminfo.part,
+                  grammems, flexia_id, ancode, lemma, method_call)
+              end
+            end
+          end
+        end
+
+        break unless gram.empty?
+      end
+
+      gram
+    end
 end
