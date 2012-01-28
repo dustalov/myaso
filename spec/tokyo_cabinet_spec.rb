5b8574abd3ee4e87e4ca3db0b29ea6afb8b21984
Dmitry A. Ustalov <dmitry@eveel.ru>
2012-01-29 00:13:17 +0600

    Myaso::TokyoCabinet stub

---
+++ spec/tokyo_cabinet_spec.rb
@@ -0,0 +1,10 @@
+# encoding: utf-8
+
+require 'tokyocabinet'
+require File.expand_path('../spec_helper', __FILE__)
+require 'myaso/tokyo_cabinet'
+
+module Myaso
+  describe TokyoCabinet do
+  end
+end
