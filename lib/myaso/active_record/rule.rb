eac332cfd92e6cb8f7b2efbc96c7f5cf8e3ea292
Dmitry A. Ustalov <dmitry@eveel.ru>
2012-01-29 00:14:00 +0600

    Myaso::ActiveRecord models

---
+++ lib/myaso/active_record/rule.rb
@@ -0,0 +1,12 @@
+# encoding: utf-8
+
+class Myaso::ActiveRecord::Rule < ActiveRecord::Base
+  def self.table_name
+    'myaso_rules'
+  end
+
+  has_many :words, class_name: '::Myaso::ActiveRecord::Word'
+
+  validates :rule_set_id, presence: true
+  validates :msd, presence: true
+end
---
+++ lib/myaso/active_record/stem.rb
@@ -0,0 +1,12 @@
+# encoding: utf-8
+
+class Myaso::ActiveRecord::Stem < ActiveRecord::Base
+  def self.table_name
+    'myaso_stems'
+  end
+
+  has_many :words, class_name: '::Myaso::ActiveRecord::Word'
+
+  validates :stem, presence: true
+  validates :rule_set_id, presence: true
+end
