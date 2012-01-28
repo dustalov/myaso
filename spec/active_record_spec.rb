22e87ff9a501a7378fcb64b97a8b300787d881c0
Dmitry A. Ustalov <dmitry@eveel.ru>
2012-01-29 00:13:04 +0600

    Myaso::ActiveRecord stub

---
+++ spec/active_record_spec.rb
@@ -0,0 +1,10 @@
+# encoding: utf-8
+
+require 'active_record'
+require File.expand_path('../spec_helper', __FILE__)
+require 'myaso/active_record'
+
+module Myaso
+  describe ActiveRecord do
+  end
+end
