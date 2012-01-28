ee37e9bb7299c5afabde7e316d58d8f9afb86f73
Dmitry A. Ustalov <dmitry@eveel.ru>
2012-01-29 00:05:39 +0600

    Load spec/support/**/*.rb

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,7 @@
 # encoding: utf-8
 
+require 'rubygems'
+
 $:.unshift File.expand_path('../../lib', __FILE__)
 
 require 'myaso'
@@ -7,5 +9,7 @@ require 'myaso'
 if RUBY_VERSION == '1.8'
   gem 'minitest'
 end
+
+Dir[File.expand_path('../support/**/*.rb', __FILE__)].each { |f| require f }
 
 require 'minitest/autorun'
