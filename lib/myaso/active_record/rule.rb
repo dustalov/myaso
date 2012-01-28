e036a2a2118554a62e1287719f286b96e8fe8580
Dmitry A. Ustalov <dmitry@eveel.ru>
2012-01-29 00:15:01 +0600

    Add :inverse_of to Rule and Stem models

--- lib/myaso/active_record/rule.rb
+++ lib/myaso/active_record/rule.rb
@@ -5,7 +5,8 @@ class Myaso::ActiveRecord::Rule < ActiveRecord::Base
     'myaso_rules'
   end
 
-  has_many :words, class_name: '::Myaso::ActiveRecord::Word'
+  has_many :words, class_name: '::Myaso::ActiveRecord::Word',
+    :inverse_of => :rule
 
   validates :rule_set_id, presence: true
   validates :msd, presence: true
--- lib/myaso/active_record/stem.rb
+++ lib/myaso/active_record/stem.rb
@@ -5,7 +5,8 @@ class Myaso::ActiveRecord::Stem < ActiveRecord::Base
     'myaso_stems'
   end
 
-  has_many :words, class_name: '::Myaso::ActiveRecord::Word'
+  has_many :words, class_name: '::Myaso::ActiveRecord::Word',
+    :inverse_of => :stem
 
   validates :stem, presence: true
   validates :rule_set_id, presence: true
