a11d44d498128433df85966796d1aadfbf812680
Dmitry A. Ustalov <dmitry@eveel.ru>
2012-01-29 00:10:47 +0600

    Introduce multiple backends: ActiveRecord & TokyoCabinet

--- lib/myaso.rb
+++ lib/myaso.rb
@@ -4,3 +4,7 @@ require 'myaso/version'
 
 module Myaso
 end
+
+require 'myaso/base'
+require 'myaso/active_record' if defined? ::ActiveRecord::Base
+require 'myaso/tokyo_cabinet' if defined? ::TokyoCabinet::TDB
