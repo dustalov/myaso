e5c05e419f15ccc0e398cfdc7e03c26b4c4fe7dc
Dmitry A. Ustalov <dmitry@eveel.ru>
2012-02-01 21:33:22 +0600

    We have to require MSD

--- lib/myaso.rb
+++ lib/myaso.rb
@@ -5,6 +5,7 @@ require 'myaso/version'
 module Myaso
 end
 
+require 'myaso/msd'
 require 'myaso/base'
 require 'myaso/active_record' if defined? ::ActiveRecord::Base
 require 'myaso/tokyo_cabinet' if defined? ::TokyoCabinet::TDB
