4336e43abf569f8813fcadcf20ecbdf2ba1c78c6
Dmitry A. Ustalov <dmitry@eveel.ru>
2012-02-07 01:48:53 +0600

    All Your Base Are Belong To Us

---
+++ spec/support/all_your_base_are_belong_to_us.rb
@@ -0,0 +1,8 @@
+# encoding: utf-8
+
+class Myaso::AllYourBaseAreBelongToUs < Myaso::Base
+  class Prefixes < Myaso::Base::Adapter; end
+  class Stems < Myaso::Base::Adapter; end
+  class Rules < Myaso::Base::Adapter; end
+  class Words < Myaso::Base::Adapter; end
+end
