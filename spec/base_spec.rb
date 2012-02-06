c4d219d44c158ebb817ae8cb79b5edc054c96bca
Dmitry A. Ustalov <dmitry@eveel.ru>
2012-02-07 01:49:27 +0600

    Tests for Base & Base::Adapter

--- spec/base_spec.rb
+++ spec/base_spec.rb
@@ -4,5 +4,23 @@ require File.expand_path('../spec_helper', __FILE__)
 
 module Myaso
   describe Base do
+    subject { Myaso::Base }
+
+    it 'must not be instantinated' do
+      proc { subject.new }.must_raise RuntimeError
+    end
+  end
+
+  describe AllYourBaseAreBelongToUs do
+    subject { Myaso::AllYourBaseAreBelongToUs.new }
+
+    [ :prefixes, :rules, :stems, :words ].each do |name|
+      it "has #{name} adapter" do
+        subject.must_respond_to name
+
+        klass = subject.class.const_get(name.to_s.capitalize)
+        subject.public_send(name).must_be_kind_of klass
+      end
+    end
   end
 end
