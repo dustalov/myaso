909a1b147687f5fa2e34c506f1a2da9766be9100
Dmitry A. Ustalov <dmitry@eveel.ru>
2012-02-06 19:37:24 +0600

    Use SimpleCov

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -10,6 +10,9 @@ if RUBY_VERSION == '1.8'
   gem 'minitest'
 end
 
+require 'simplecov'
+SimpleCov.start
+
 Dir[File.expand_path('../support/**/*.rb', __FILE__)].each { |f| require f }
 
 require 'minitest/autorun'
