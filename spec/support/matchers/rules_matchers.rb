98fe736ce166e9663d6a941f08e5ea7a52c0fdf9
Dmitry A. Ustalov <dmitry@eveel.ru>
2012-03-23 00:30:40 +0600

    Rules setters matchers

--- spec/support/matchers/rules_matchers.rb
+++ spec/support/matchers/rules_matchers.rb
@@ -3,17 +3,37 @@
 class MiniTest::Unit::TestCase
   def self.should_behave_like_a_rules!
     describe '#get' do
-      it 'should fetch a prefix' do
+      it 'should fetch a rule' do
         subject.find(1).must_equal('rule_set_id' => '1')
       end
 
-      it 'should fetch a prefix in UTF-8' do
+      it 'should fetch a rule in UTF-8' do
         subject.find(2).must_equal('rule_set_id' => '1',
                                    'suffix' => 's')
       end
 
-      it 'should return nil when prefix is absent' do
+      it 'should return nil when rule is absent' do
         subject.find(3).must_be_nil
+      end
+    end
+
+    describe '#set' do
+      let(:rule) { { 'rule_set_id' => '1' } }
+
+      it 'should set an empty rule' do
+        subject.find(3).must_be_nil
+        subject.set(3, rule)
+        subject.find(3).must_equal rule
+      end
+
+      it 'should replace an existent rule' do
+        new_rule = { 'rule_set_id' => '2' }
+
+        subject.find(3).must_be_nil
+        subject.set(3, rule)
+        subject.find(3).must_equal rule
+        subject.set(3, new_rule)
+        subject.find(3).must_equal new_rule
       end
     end
   end
