05910093c226aaa2c82b3978b9d471487d58d859
Dmitry A. Ustalov <dmitry@eveel.ru>
2012-03-25 15:29:02 +0600

    I guess, words_spec should test words

--- spec/tokyo_cabinet/words_spec.rb
+++ spec/tokyo_cabinet/words_spec.rb
@@ -7,7 +7,7 @@ class Myaso::TokyoCabinet
     let(:tmpdir) { Dir.mktmpdir }
     let(:myaso) { Myaso::TokyoCabinet.new(tmpdir, :manage) }
 
-    subject { myaso.prefixes }
+    subject { myaso.words }
 
     before { populate_tokyo_cabinet! myaso }
 
