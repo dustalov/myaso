8600c8f7be7085436554f2dae3e06c39aedf8cd1
Dmitry Ustalov <dmitry@eveel.ru>
2013-04-21 21:06:57 +0600

    Refine CLI tests

--- spec/bin_spec.rb
+++ spec/bin_spec.rb
@@ -10,33 +10,32 @@ describe 'CLI' do
 
   it 'should print version information' do
     invoke('-v').must_include 'Myaso v%s' % Myaso::VERSION
-    invoke('--version').must_include 'Myaso v%s' % Myaso::VERSION
+    invoke('--version').must_equal invoke('-v')
   end
 
   it 'should print help' do
     invoke('-h').must_include 'Commands:'
     invoke('-h').must_include 'Options:'
-
-    invoke('--help').must_include 'Commands:'
-    invoke('--help').must_include 'Options:'
+    invoke('--help').must_equal invoke('-h')
   end
 
   it 'should evaluate parameters' do
     invoke('-e', 'puts :hi; exit').must_include 'hi'
-    invoke('--eval', 'puts :hi; exit').must_include 'hi'
+    invoke('--eval', 'puts :hi; exit').must_equal(
+      invoke('-e', 'puts :hi; exit'))
   end
 
   it 'should consider the ngrams parameter' do
     invoke('-n', 'test', '-e', 'puts options.ngrams; exit').
       must_include 'test'
     invoke('--ngrams', 'test', '-e', 'puts options.ngrams; exit').
-      must_include 'test'
+      must_equal(invoke('-n', 'test', '-e', 'puts options.ngrams; exit'))
   end
 
   it 'should consider the lexicon parameter' do
     invoke('-l', 'test', '-e', 'puts options.lexicon; exit').
       must_include 'test'
     invoke('--lexicon', 'test', '-e', 'puts options.lexicon; exit').
-      must_include 'test'
+      must_equal(invoke('-l', 'test', '-e', 'puts options.lexicon; exit'))
   end
 end
