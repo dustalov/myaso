a4acebd97aab06cc99821a6761fa996d528bb119
Dmitry Ustalov <dmitry@eveel.ru>
2013-05-03 16:31:45 +0600

    Improve the Ngrams #[] and #[]= arguments parsing

--- lib/myaso/ngrams.rb
+++ lib/myaso/ngrams.rb
@@ -21,18 +21,16 @@ class Myaso::Ngrams
 
   # Obtain the count of the specified unigram, bigram, or trigram.
   #
-  def [] *ngram
-    u, b, t = ngram
-    return 0 unless table.include? u
-    return 0 unless table[u].include? b
-    table[u][b][t]
+  def [] unigram, bigram = nil, trigram = nil
+    return 0 unless table.include? unigram
+    return 0 unless table[unigram].include? bigram
+    table[unigram][bigram][trigram]
   end
 
   # Assign the count to the specified unigram, bigram, or trigram.
   #
-  def []= *ngram, count
-    u, b, t = ngram
-    table[u][b][t] = count
+  def []= unigram, bigram = nil, trigram = nil, count
+    table[unigram][bigram][trigram] = count
   end
 
   # Two storages are equal iff they tables are equal.
